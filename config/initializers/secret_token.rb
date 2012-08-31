58ea174ad8faad9a1d3a42305089bfef1037c61b
Seech <seechinsu@gmail.com>
2012-08-30 23:17:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seech::Application.config.secret_token = 'e62241afc0d97f33500ea512277356d2b0f2851e653889981ef0535247dae0320771a5c4ba2027e3838f3a752fae019fffbac408ae1f60fee87d156e9ced150a'
